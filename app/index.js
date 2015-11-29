32d6955599e69126e4a5e102b3b609352c6e548c
sohamkamani <sohamkamani@gmail.com>
2015-11-29 14:31:53 +0530

    init tutorial

---
+++ app/index.js
@@ -0,0 +1,18 @@
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+  // The name `constructor` is important here
+  constructor: function () {
+    // Calling the super constructor is important so our generator is correctly set up
+    generators.Base.apply(this, arguments);
+
+    // Next, add your custom code
+    this.option('coffee'); // This method adds support for a `--coffee` flag
+  },
+  method1: function () {
+    console.log('method 1 just ran');
+  },
+  method2: function () {
+    console.log('method 2 just ran');
+  }
+});
