e502fd0270934bec0634f4c03ea0ecd2f3c3eac0
sohamkamani <sohamkamani@gmail.com>
2015-11-30 14:36:36 +0530

    added prompt

--- app/index.js
+++ app/index.js
@@ -5,15 +5,39 @@ module.exports = generators.Base.extend({
   constructor: function () {
     // Calling the super constructor is important so our generator is correctly set up
     generators.Base.apply(this, arguments);
-    
-
-    // Next, add your custom code
-    this.option('coffee'); // This method adds support for a `--coffee` flag
+    this.prompt([{
+			name: 'moduleName',
+			message: 'What do you want to name your module?',
+			default: this.appname.replace(/\s/g, '-'),
+		}, {
+			name: 'githubUsername',
+			message: 'What is your GitHub username?',
+			store: true,
+		}, {
+			name: 'website',
+			message: 'What is the URL of your website?',
+			store: true
+		}, {
+			name: 'cli',
+			message: 'Do you need a CLI?',
+			type: 'confirm',
+			default: false
+		}], console.log);
   },
-  method1: function () {
-    console.log('method 1 just ran');
+  writing: function () {
+    this.fs.copyTpl(
+      this.templatePath('index.js'),
+      this.destinationPath('public/index.js'), {
+        someJs: 'Templating with Yeoman'
+      }
+    );
   },
-  method2: function () {
-    console.log('method 2 just ran');
+  gitInit: function () {
+    this.spawnCommandSync('git', ['init']);
+  },
+  installDeps: function () {
+    this.installDependencies({
+      bower: false
+    });
   }
 });
