052608f56b6cb8f811f1e3d1522b38300866f29c
sohamkamani <sohamkamani@gmail.com>
2015-11-30 12:59:27 +0530

    Some git commit message
    testing multiline commit
    * Checking if bullet points work
    * Same
    
    Ending paragraph

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ module.exports = generators.Base.extend({
   constructor: function () {
     // Calling the super constructor is important so our generator is correctly set up
     generators.Base.apply(this, arguments);
+    
 
     // Next, add your custom code
     this.option('coffee'); // This method adds support for a `--coffee` flag
