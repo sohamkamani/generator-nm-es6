1ff96c205a545ddfba907b17aaa05647f828395a
sohamkamani <sohamkamani@gmail.com>
2016-02-10 12:54:08 +0530

    Added more generic description and removed old templates

--- app/oldtemplates/test/index.spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import {expect} from 'chai';
-import hello from '../source';
-
-describe('Hello World', ()=>{
-  it('Outputs hello world', ()=>{
-    expect(hello('world')).to.equal('Hello world!');
-  });
-});
